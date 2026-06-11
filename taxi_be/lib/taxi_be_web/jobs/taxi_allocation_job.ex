77843bf3c5d9189fd617bbb11123a59980e070ac
Yukon <protectthecloud@gmail.com>
2026-06-11 13:57:02 -0600

    Implementar version 2 paralela taxi allocation

--- taxi_be/lib/taxi_be_web/jobs/taxi_allocation_job.ex
+++ taxi_be/lib/taxi_be_web/jobs/taxi_allocation_job.ex
@@ -1,121 +1,164 @@
 defmodule TaxiBeWeb.TaxiAllocationJob do
   use GenServer
 
-  @driver_response_timeout 60_000
+  @allocation_timeout 90_000
   @estimated_arrival_time "5 minutos"
+  @drivers_to_contact 3
 
   def start_link(request, name) do
     GenServer.start_link(__MODULE__, request, name: name)
   end
 
   def init(request) do
-    Process.send(self(), :contact_next_driver, [:nosuspend])
+    Process.send(self(), :contact_drivers, [:nosuspend])
 
     {:ok,
      %{
        request: request,
-       candidates: candidate_taxis(),
-       current_taxi: nil,
+       contacted_drivers: [],
+       pending_drivers: [],
+       accepted_driver: nil,
        timer: nil,
        phase: :allocating
      }}
   end
 
-  def handle_info(:contact_next_driver, %{candidates: []} = state) do
+  def handle_info(:contact_drivers, %{request: request} = state) do
+    drivers =
+      candidate_taxis()
+      |> Enum.take(@drivers_to_contact)
+
+    Enum.each(drivers, fn driver ->
+      notify_driver(driver.nickname, %{
+        msg: booking_message(request),
+        bookingId: request["booking_id"]
+      })
+    end)
+
+    timer = Process.send_after(self(), :allocation_timeout, @allocation_timeout)
+
+    {:noreply,
+     %{
+       state
+       | contacted_drivers: drivers,
+         pending_drivers: Enum.map(drivers, & &1.nickname),
+         timer: timer,
+         phase: :allocating
+     }}
+  end
+
+  def handle_info(:allocation_timeout, %{phase: :allocating} = state) do
     notify_customer(
       state.request,
       "No fue posible encontrar un taxi disponible para tu solicitud."
     )
 
-    {:stop, :normal, state}
+    {:stop, :normal, %{state | phase: :failed, timer: nil}}
   end
 
-  def handle_info(:contact_next_driver, %{candidates: [taxi | remaining_taxis]} = state) do
-    forward_request_to_driver(state.request, taxi)
-
-    timer =
-      Process.send_after(
-        self(),
-        {:driver_timeout, taxi.nickname},
-        @driver_response_timeout
-      )
-
-    {:noreply,
-     %{
-       state
-       | candidates: remaining_taxis,
-         current_taxi: taxi,
-         timer: timer,
-         phase: :waiting_driver_response
-     }}
-  end
-
-  def handle_info(
-        {:driver_timeout, taxi_nickname},
-        %{current_taxi: %{nickname: taxi_nickname}, phase: :waiting_driver_response} = state
-      ) do
-    Process.send(self(), :contact_next_driver, [:nosuspend])
-
-    {:noreply, %{state | current_taxi: nil, timer: nil}}
-  end
-
-  def handle_info({:driver_timeout, _taxi_nickname}, state) do
+  def handle_info(:allocation_timeout, state) do
     {:noreply, state}
   end
 
-  def handle_cast(
-        {:process_accept, username},
-        %{current_taxi: %{nickname: username}, phase: :waiting_driver_response} = state
-      ) do
-    cancel_timer(state.timer)
+  def handle_cast({:process_accept, username}, %{phase: :allocating} = state) do
+    if username in state.pending_drivers do
+      cancel_timer(state.timer)
 
-    notify_customer(
-      state.request,
-      "Tu taxi #{username} está en camino. Tiempo estimado de llegada: #{@estimated_arrival_time}."
-    )
+      notify_customer(
+        state.request,
+        "Tu taxi #{username} está en camino. Tiempo estimado de llegada: #{@estimated_arrival_time}."
+      )
 
-    {:stop, :normal, %{state | phase: :accepted, timer: nil}}
+      notify_other_drivers(state.pending_drivers, username, %{
+        msg: "El viaje ya fue aceptado por otro conductor.",
+        bookingId: state.request["booking_id"]
+      })
+
+      {:stop,
+       :normal,
+       %{
+         state
+         | accepted_driver: username,
+           phase: :accepted,
+           timer: nil,
+           pending_drivers: []
+       }}
+    else
+      {:noreply, state}
+    end
   end
 
-  def handle_cast(
-        {:process_reject, username},
-        %{current_taxi: %{nickname: username}, phase: :waiting_driver_response} = state
-      ) do
-    cancel_timer(state.timer)
-    Process.send(self(), :contact_next_driver, [:nosuspend])
+  def handle_cast({:process_accept, _username}, state) do
+    {:noreply, state}
+  end
 
-    {:noreply, %{state | current_taxi: nil, timer: nil}}
+  def handle_cast({:process_reject, username}, %{phase: :allocating} = state) do
+    new_pending_drivers = List.delete(state.pending_drivers, username)
+
+    if Enum.empty?(new_pending_drivers) do
+      cancel_timer(state.timer)
+
+      notify_customer(
+        state.request,
+        "No fue posible encontrar un taxi disponible para tu solicitud."
+      )
+
+      {:stop,
+       :normal,
+       %{
+         state
+         | phase: :failed,
+           timer: nil,
+           pending_drivers: []
+       }}
+    else
+      {:noreply, %{state | pending_drivers: new_pending_drivers}}
+    end
+  end
+
+  def handle_cast({:process_reject, _username}, state) do
+    {:noreply, state}
   end
 
   def handle_cast(_message, state) do
     {:noreply, state}
   end
 
-  defp forward_request_to_driver(request, taxi) do
+  defp booking_message(request) do
     %{
       "pickup_address" => pickup_address,
-      "dropoff_address" => dropoff_address,
-      "booking_id" => booking_id
+      "dropoff_address" => dropoff_address
     } = request
 
-    TaxiBeWeb.Endpoint.broadcast(
-      "driver:" <> taxi.nickname,
-      "booking_request",
-      %{
-        msg: "Viaje de '#{pickup_address}' a '#{dropoff_address}'",
-        bookingId: booking_id
-      }
-    )
+    "Viaje de '#{pickup_address}' a '#{dropoff_address}'"
+  end
+
+  defp notify_driver(username, payload) do
+    case :global.whereis_name({:driver_channel, username}) do
+      :undefined ->
+        :driver_not_connected
+
+      pid ->
+        send(pid, {:booking_request, payload})
+    end
   end
 
   defp notify_customer(request, message) do
     %{"username" => username} = request
 
-    TaxiBeWeb.Endpoint.broadcast(
-      "customer:" <> username,
-      "booking_request",
-      %{msg: message}
-    )
+    case :global.whereis_name({:customer_channel, username}) do
+      :undefined ->
+        :customer_not_connected
+
+      pid ->
+        send(pid, {:booking_request, %{msg: message, bookingId: request["booking_id"]}})
+    end
+  end
+
+  defp notify_other_drivers(pending_drivers, accepted_driver, payload) do
+    pending_drivers
+    |> Enum.reject(fn username -> username == accepted_driver end)
+    |> Enum.each(fn username -> notify_driver(username, payload) end)
   end
 
   defp cancel_timer(nil), do: :ok
@@ -128,8 +171,8 @@ defmodule TaxiBeWeb.TaxiAllocationJob do
   def candidate_taxis() do
     [
       %{nickname: "frodo", latitude: 19.0319783, longitude: -98.2349368},
-      %{nickname: "samwise", latitude: 19.0061167, longitude: -98.2697737},
-      %{nickname: "pippin", latitude: 19.0092933, longitude: -98.2473716}
+      %{nickname: "pippin", latitude: 19.0092933, longitude: -98.2473716},
+      %{nickname: "samwise", latitude: 19.0061167, longitude: -98.2697737}
     ]
   end
 end
